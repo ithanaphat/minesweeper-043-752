2f6ecb452c6f6393b0948d65fc7455cdd1604b8b
kittiphob subsorn <slaaxx1122@gmail.com>
2025-02-18 11:13:55 +0700

    commit 1

--- game/src/main/java/com/lab/Minesweeper.java
+++ game/src/main/java/com/lab/Minesweeper.java
@@ -30,15 +30,41 @@ public class Minesweeper {
 
     void displayField() {
         // Task 1: Display the mine field to terminal
+        for (int i=0; i<fieldX; i++) {
+            for (int j=0; j<fieldY; j++) {
+                if(cells[i][j] == IS_SAFE){
+                    System.out.print(".");
+                }else {
+                    System.out.print("X");
+                }
+            }
+            System.out.println("");
     }
+}
+
+
     void setMineCell(int x, int y) {
         cells[x][y] = IS_MINE;
     }
 
     void initFromFile(String mineFieldFile) {
         InputStream is = getClass().getClassLoader().getResourceAsStream(mineFieldFile);
-        
+        Scanner scan = new Scanner(is);
+        fieldX = Integer.parseInt(scan.nextLine());
+        fieldY = Integer.parseInt(scan.nextLine());
+        cells = new int[fieldX][fieldY];
+ 
+        for(int i = 0 ; i < fieldX;i++){
+            String line = scan.nextLine();
+            for(int j = 0 ; j < fieldY;j++){
+                if(line.charAt(j) == 'X'){
+                    cells[i][j] = IS_MINE;
+                }else if(line.charAt(j) == '.')
+                    cells[i][j] = IS_SAFE;
+            }
+        }
+ 
         // Task 2: Using `java.util.Scanner` to load mine field from the input stream named, `is`
-
+ 
     }
 }
